6f63773ed5719138bfd97ba954f2f887b1cd464c
pavelsmirnov77 <pavelsmirnov35rus@yandex.ru>
2022-06-21 18:41:30 +0300

    Выполнено задание по лекции "Базовый синтаксис"

--- base-syntax/src/ru/sber/base/syntax/Task4.java
+++ base-syntax/src/ru/sber/base/syntax/Task4.java
@@ -9,18 +9,14 @@ public class Task4 {
         }
         System.out.println();
         double max = array[0];
-        double min = array[0];
         double sum = 0;
         for (int i = 0; i < array.length; i++) {
             if(max < array[i])
                 max = array[i];
-            if(min > array[i])
-                min = array[i];
             sum += array[i];
         }
         double avg = sum/array.length;
         System.out.println("Максимальное значение: " + max);
-        System.out.println("Минимальное значение: " + min);
         System.out.println("Среднее значение: " + avg);
     }
 }
