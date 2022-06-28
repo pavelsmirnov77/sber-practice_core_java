fe54e20d7927ee69a3938bd35b1dc6a8f870be01
pavelsmirnov77 <pavelsmirnov35rus@yandex.ru>
2022-06-28 20:09:13 +0300

    Выполнено задание по лекции "Коллекции (Часть 1)"

---
+++ Collections 1/src/ru/sber/collections1/LinkedListIterator.java
@@ -0,0 +1,29 @@
+package ru.sber.collections1;
+import java.util.*;
+
+public class LinkedListIterator implements Iterator {
+
+    private LinkedList linkedList;
+    private int i;
+
+    public LinkedListIterator(LinkedList linkedList) {
+        this.linkedList = linkedList;
+        this.i = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return i < linkedList.size();
+    }
+
+    @Override
+    public Object next() throws NoSuchElementException {
+        if (hasNext()) {
+            i++;
+            return linkedList.get(i-1);
+        }
+        else {
+            throw new NoSuchElementException("В полносвязном списке нет элементов");
+        }
+    }
+}
