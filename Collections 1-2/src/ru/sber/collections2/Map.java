cf172c31a2afa5a62535c6c4014e18b0e6354af7
pavelsmirnov77 <pavelsmirnov35rus@yandex.ru>
2022-06-30 04:15:56 +0300

    Выполнено задание по лекции "Коллекции (Часть 2)"

---
+++ Collections 1-2/src/ru/sber/collections2/Map.java
@@ -0,0 +1,17 @@
+package ru.sber.collections2;
+
+import ru.sber.collections1.Collection;
+
+public interface Map {
+    int size();
+    boolean isEmpty();
+    boolean containsKey(Object key);
+    boolean containsValue(Object value);
+    Object get(Object key);
+    Object put(Object key, Object value);
+    Object remove(Object key);
+    void clear();
+    Collection values();
+    Collection keySet();
+    Collection entrySet();
+}
