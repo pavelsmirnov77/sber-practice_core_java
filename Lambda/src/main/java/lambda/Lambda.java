e212b8928176f9d82f259b7e1339a9b3d871f734
Pavel Smirnov <64857615+pavelsmirnov77@users.noreply.github.com>
2022-07-11 03:18:32 +0300

    Удален метод main, исправлен метод methodConsumer

---
+++ Lambda/src/main/java/lambda/Lambda.java
@@ -0,0 +1,34 @@
+package lambda;
+
+import java.util.function.*;
+
+public class Lambda {
+    public static String methodOne(ZeroArgsFunctionalInterface zeroInterface) {
+        return zeroInterface.zeroArgsMethod();
+    }
+
+    public static double methodTwo(double a, OneArgFunctionalInterface oneInterface) {
+        return oneInterface.oneArgMethod(a);
+    }
+
+    public static double methodThree(double a, double b, TwoArgsFunctionalInterface twoInterface) {
+        return twoInterface.twoArgsMethod(a, b);
+    }
+
+    public static String methodFunction(String a, Function<String, String> function) {
+        return function.apply(a);
+    }
+
+    public static Integer methodIntSupplier(IntSupplier intSupplier) {
+        return intSupplier.getAsInt();
+    }
+
+    public static boolean methodConsumer(Consumer consumer) {
+        consumer.equals(3);
+        return false;
+    }
+
+    public static boolean methodPredicate(String a, Predicate<String> b) {
+        return b.test(a);
+    }
+}
